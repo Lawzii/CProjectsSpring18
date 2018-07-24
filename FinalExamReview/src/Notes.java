50eb89bace3b844ce592ca32a1f38150749880e6
Lawzii <gowanalyssar@gmail.com>
2018-07-24 19:04:17 -0400

    Add files via upload

---
+++ FinalExamReview/src/Notes.java
@@ -0,0 +1,67 @@
+
+public class Notes {
+
+	
+	/*  Module 1 > 
+	 * 	Output / Input 
+	 * 	Variables (Types, Size, Heirachy)
+	 * 			String (how it works w/ memory)
+	 * 			double > float > byte
+	 * 	Selections / Branching 
+	 * 		if, if-else, if-else-if, switch (structure & breaks)
+	 * 
+	 * 
+	 * Module 2 >
+	 * 	Loops / Tracing Code
+	 * 		For, While, Do-While, Enhanced For
+	 * 	Arrays
+	 * 		Create
+	 * 		Single (how it's used)
+	 *  	2D (read / trace)
+	 *  Methods (Call, creating, return, static vs. non-static, scope / visibility)
+	 *  
+	 *  
+	 * Module 3 >
+	 * 	Classes
+	 * 		objects vs class (classes are blueprints used to create objects through constructors)
+	 * 		constructors ( can have multiples) (constructors are defined by class name)
+	 * 		setters & getters
+	 * 		private (self) / public (everyone) / protected (team) [visibility]
+	 * 		overloading (within a class that has multiple methods with different arguments) 
+	 * 		overwriting (when you inherit from a parent class and change it [i.e. toString])
+	 * 	Exception handling (try/catch, basic)
+	 * 	>NO file processing<
+	 * 
+	 * 
+	 * Module 4 >
+	 * 	Interface
+	 * 		Abstract methods
+	 * 		implementation of interfaces
+	 * 	Abstract classes
+	 * 		Extension
+	 * 	Inheritance
+	 * 		sub-classes
+	 * 		visibility (what inherits what)
+	 * 	"Family Tree"
+	 * 
+	 * 
+	 * Housekeeping >
+	 * 	Java standards
+	 * 		Naming (Classes are capital, variables are lowercase)
+	 * 		Logic ( || / && has a higher precedence)
+	 * 	Terms
+	 * 		Encapsulation - data corruption / one path
+	 * 		Polymorphism - 
+	 * 	Math (Integers + Doubles, Truncation, Promotion)
+	 * 	Prefix / Postfix operators (++i, i++)
+	 * 
+	 * 
+	 *  
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
